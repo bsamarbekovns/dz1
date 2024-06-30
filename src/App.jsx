9ff3084f30df4a1cc99dae2ea3967b97120cf7a7
Бермет Самарбекова <bsamarbekovns@gmail.com>
2024-06-30 14:37:08 +0300

    dz1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main.jsx";
+
+
+function App() {
+
+  return (
+   <div>
+     <Main text="привет"/>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+
+
+const Main = ({text}) => {
+
+    console.log(text)
+    return (
+        <div>
+            {text}
+        </div>
+    );
+};
+
+export default Main;
+
+
